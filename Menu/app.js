85727bb08c31f115142c6362f685ecaecb2203a7
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-09-21 16:13:27 +1000

    add menu buttons dynamically

--- Menu/app.js
+++ Menu/app.js
@@ -1,3 +1,7 @@
+// get only unique categories
+// iterate over categories return buttons
+// make sure to select buttons when they are available
+
 const menu = [
 	{
 		id: 1,
@@ -71,34 +75,24 @@ const menu = [
 		img: "./images/item-9.jpeg",
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
 	},
+	{
+		id: 10,
+		title: "steak dinner",
+		category: "dinner",
+		price: 25.99,
+		img: "./images/item-10.jpeg",
+		desc: `lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+	},
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const btnContainer = document.querySelector(".btn-container");
 
 // load items
 window.addEventListener("DOMContentLoaded", function () {
 	// console.log("shake and bake");
 	displayMenuItems(menu);
-});
-
-// filter items
-filterBtns.forEach(function (btn) {
-	btn.addEventListener("click", function (e) {
-		// console.log(e.currentTarget.dataset.id);
-		const category = e.currentTarget.dataset.id;
-		const menuCategory = menu.filter(function (menuItem) {
-			if (category === menuItem.category) {
-				return menuItem;
-			}
-		});
-		// console.log(menuCategory);
-		if (category === "all") {
-			displayMenuItems(menu);
-		} else {
-			displayMenuItems(menuCategory);
-		}
-	});
+	displayBtns();
 });
 
 function displayMenuItems(menuItems) {
@@ -119,4 +113,44 @@ function displayMenuItems(menuItems) {
 	displayMenu = displayMenu.join("");
 	// console.log(displayMenu);
 	sectionCenter.innerHTML = displayMenu;
+}
+
+function displayBtns() {
+	const categories = menu.reduce(
+		function (values, item) {
+			// console.log(item);
+			if (!values.includes(item.category)) {
+				values.push(item.category);
+			}
+			return values;
+		},
+		["all"]
+	);
+	// console.log(categories);
+	const categoryBtns = categories
+		.map(function (category) {
+			return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+		})
+		.join("");
+	// console.log(categoryBtns);
+	btnContainer.innerHTML = categoryBtns;
+	const filterBtns = document.querySelectorAll(".filter-btn");
+	// filter items
+	filterBtns.forEach(function (btn) {
+		btn.addEventListener("click", function (e) {
+			// console.log(e.currentTarget.dataset.id);
+			const category = e.currentTarget.dataset.id;
+			const menuCategory = menu.filter(function (menuItem) {
+				if (category === menuItem.category) {
+					return menuItem;
+				}
+			});
+			// console.log(menuCategory);
+			if (category === "all") {
+				displayMenuItems(menu);
+			} else {
+				displayMenuItems(menuCategory);
+			}
+		});
+	});
 }
