b52d6e2b2a2bbd8fe5ed0522e31938ca3ca0abd3
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-10-05 14:32:53 +1100

    tabs

---
+++ Tabs/app.js
@@ -0,0 +1,22 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", (e) => {
+	// console.log(e.target);
+	// console.log(e.target.dataset.id);
+	const id = e.target.dataset.id;
+	if (id) {
+		// remove "active" from other buttons
+		btns.forEach((btn) => {
+			btn.classList.remove("active");
+			e.target.classList.add("active");
+		});
+		// hide other articles
+		articles.forEach((article) => {
+			article.classList.remove("active");
+		});
+		const element = document.getElementById(id);
+		element.classList.add("active");
+	}
+});
